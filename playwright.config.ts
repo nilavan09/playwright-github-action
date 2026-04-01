0efe4d4bcb2d5415c4cdc314f06aa8bdb118caf8
nilavanpoz <pozhilnilavang@rarecrew.com>
2026-04-01 17:42:10 +0530

    chenged config and added argos

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,18 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    process.env.CI ? ["dot"] : ["list"],
+    [
+      "@argos-ci/playwright/reporter",
+      
+       { // Upload to Argos on CI only.
+        uploadToArgos: !!process.env.CI,
+
+      }
+    ],
+    ['html']
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -30,6 +41,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: "only-on-failure",
   },
 
   /* Configure projects for major browsers */
--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,8 +1,10 @@
 import { test, expect } from '@playwright/test';
+import { argosScreenshot } from "@argos-ci/playwright";
 
 test('1st test', async ({ page }) => {
   await page.goto("http://localhost:4200/")
     await page.getByText('Forms').click()
     await page.getByText('Form Layouts').click()
+    await argosScreenshot(page, "Forms Layoutpage");
     //test commit
 });
