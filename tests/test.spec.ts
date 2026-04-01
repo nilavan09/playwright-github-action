cc320789e64118ec833a8c3f99b3498ba6979898
nilavanpoz <pozhilnilavang@rarecrew.com>
2026-04-01 17:22:08 +0530

    github action

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -4,4 +4,5 @@ test('1st test', async ({ page }) => {
   await page.goto("http://localhost:4200/")
     await page.getByText('Forms').click()
     await page.getByText('Form Layouts').click()
+    //test commit
 });
